0b8cfb557d3d68abdd7039ddd7a6cc7957b1ff5e
Rob Girdler <robert@miscomputers.co.uk>
2019-11-03 00:17:43 +0000

    Updated the creator string and set initial bed size values. By default the profile uses a 235x235x250 bed area, however only the center 220x220 is actually heated and recommended for use by Creality. This build is designed to locate the max values for X and Y without crashing the head. We need these to allow full movement when we add the X and Y min offsets shortly.

--- Marlin/Custom Build 1.0.0/Marlin/Configuration.h
+++ Marlin/Custom Build 1.0.0/Marlin/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(thisiskeithb, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Rob Girdler, Ender-3)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -874,15 +874,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 240
+#define Y_MAX_POS 240
 #define Z_MAX_POS 250
 
 /**
